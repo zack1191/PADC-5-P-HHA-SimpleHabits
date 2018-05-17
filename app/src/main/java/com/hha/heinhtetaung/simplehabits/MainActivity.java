f8ba50f50506197bf8f6391aa7e3ebb2313f368f
HeinHtetAung <14heinhtetaung@gmail.com>
2018-05-17 14:00:27 +0630

    initial commit

---
+++ app/src/main/java/com/hha/heinhtetaung/simplehabits/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hha.heinhtetaung.simplehabits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this, this);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+        }
+    }
+}
